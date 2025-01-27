30023b93670f33d6546b5761c8b1149f32b3b71d
jypiter112 <146531209+jypiter112@users.noreply.github.com>
2025-01-27 08:48:11 +0200

    Major update
    Persistance, GET req support for javascript/css. Still unsecure
    !

--- main.cpp
+++ main.cpp
@@ -6,36 +6,99 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <vector>
+#include <sstream>
 #pragma comment(lib, "Ws2_32.lib")
 
-int SendIndex(SOCKET client) {
-    // Make response
-    std::cout << "Sending res:\n";
+using std::vector, std::string;
+SOCKET* client_p;
+int SendIndex(SOCKET, std::string); 
+
+/*
+    Basic GET req support
+    to do POST req support
+*/
+
+vector<string> ParsePostRequest(char* req){
+    vector<string> output_r;
+    string r(req);
+    r = r.substr(r.find("\n\n") + 2);
+    std::stringstream ss(r);
+    string token;
+    while(std::getline(ss, token, '&')){
+        output_r.push_back(token);
+    }
+    return output_r;
+}
+
+std::string GetPathFromReq(char* req){
+    if(strlen(req) < 15) return "";
+    std::string r(req, 25);
+    std::string path;
+    // Parse request types
+    /*
+        Supported : GET
+    */
+    if(r.substr(0, 3) == "GET"){
+        int i = r.substr(4).find(" ");
+        if(i > 0){
+            path = r.substr(4, i);
+        } else {
+            std::cout << "Invalid path\n";
+            return "";
+        }
+        return path.substr(1);
+    }
+    if(r.substr(0, 4) == "POST" || r.substr(0, 4) == "post"){
+        // Doesnt work At the moment
+        std::cout << "POST" << req << "END OF POST\n";
+        vector<string> post_data = ParsePostRequest(req);
+        for(auto i : post_data){
+            std::cout << i.substr(i.find('=') + 1) << std::endl;
+        }
+    }
+    return "";
+}
+int SendIndex(SOCKET client, std::string filename) {
+    /*
+        Insecure!!! Can send prettymuch any file back
+        To Be Fixed!
+    */
+    std::cout << "[Server] Sending res:\n";
     std::string response;
-    std::ifstream file("C:\\Users\\joona\\source\\repos\\server\\server\\index.html");
+    std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Couldnt open file\n";
         return 1;
     }
-
     std::string body = "";
     std::string line;
     while (std::getline(file, line)) {
         if(line.size() > 0) body += line + "\n";
     }
     file.close();
+    // Parse filetype requested
+    /*
+        Supported: css, js
+        everything else -> text/html
+    */
+    std::string filetype(filename.substr(filename.find('.') + 1, filename.length()));
+    std::string restype = "text/html";
+    if(filetype == "css") restype = "stylesheet";
+    if(filetype == "js") restype = "text/javascript";
     // --
     // Write response to memory
     response = "HTTP/1.0 200 OK\n";
     response += "Server: LeanAndMean\n";
     response += "Content-Length: " + std::to_string(body.length()) + "\n";
-    response += "Content-Type: text/html; charset=UTF-8\n\n";
+    response += "Content-Type: " + restype + "; charset=UTF-8\n\n";
     response += body;
 
     // Send response
     const char* final_res = response.c_str();
     send(client, final_res, strlen(final_res), 0);
     std::cout << final_res << std::endl;
+    std::cout << "[Server] End of res ----\n";
     return 0;
 }
 
@@ -81,6 +144,7 @@ int main(int argc, char** argv) {
 
         // Accept a client connection
         client = accept(sock, NULL, NULL);
+        client_p = &client;
         if (client == INVALID_SOCKET) {
             printf("Accept failed: %d\n", WSAGetLastError());
             closesocket(sock);
@@ -96,10 +160,10 @@ int main(int argc, char** argv) {
             int recvResult = recv(client, request, sizeof(request) - 1, 0);
             if (recvResult > 0) {
                 request[recvResult] = '\0'; // Null terminate
-                printf("Client > \n%s\n", request);
-                // Check if its a GET request
-                std::string fb(request, 3);
-                if (fb == "GET") SendIndex(client);
+                // printf("Client > \n%s\n", request);
+                std::string path = GetPathFromReq(request);
+                if(!path.empty()) SendIndex(client, path);
+                else std::cout << "[Client] Request:" << request << "[Client] End of request ---\n";
             }
             else if (recvResult == 0) {
                 printf("Connection closed by client.\n");
